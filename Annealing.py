b8b162ba6a315e5e24bc0693f95e709cdbeac933
Asher Lantz <lantza15@Asher.local>
2018-06-26 14:54:30 -0400

    Preliminary Monte Carlo

--- Annealing.py
+++ Annealing.py
@@ -15,8 +15,11 @@ def Energy_Array(lattice,basis):
     _Energy_Array = np.array([State_Energy(lattice,basis,i) for i in range(basis.M)])
     return _Energy_Array
 
+def NN_State_Probability(J_Beta,_State_Energy):
+    return math.exp(-1 * J_Beta * _State_Energy)
+
 def Probability_Array(basis,_Energy_Array,J_Beta):
-    NN_Probability_Array = np.array([math.exp(-1 * J_Beta * _Energy_Array[i]) for i in range(basis.M)])
+    NN_Probability_Array = np.array([NN_State_Probability(J_Beta,_Energy_Array[i]) for i in range(basis.M)])
     Z = sum(NN_Probability_Array)
     _Probability_Array = np.array([NN_Probability_Array[i] / Z for i in range(basis.M)])
     return _Probability_Array
@@ -74,23 +77,48 @@ def Tower_Sample_Average_Magnetization_Sqrd(_Probability_Array,Magnetization_Sqr
     fileD.close()
     return sum(NN_Mag_Array) / Sample_Size
 
+def Monte_Carlo_Sampling(lattice,basis,J_Beta,Sample_Size):
+    # fileU = open("MCEnergyPerUpdate.txt","w")
+    # fileS = open("MCEnergyVsSampleSize.txt","w")
+    Energy_Array = np.zeros(0)
+    state = basis.state(1)
+    State_Array = state.reshape((1,state.size))
+    for i in range(Sample_Size):
+        index = np.random.randint(0,state.size)
+        _State_Energy = State_Energy(lattice,basis,basis.index(state))
+        Prob_Beta = NN_State_Probability(J_Beta,_State_Energy)
+        basis.flip(state,index)
+        _State_Energy = State_Energy(lattice,basis,basis.index(state))
+        Prob_Alpha = NN_State_Probability(J_Beta,_State_Energy)
+        probability_append = min(1,Prob_Alpha / Prob_Beta)
+        if np.random.rand() <= probability_append:
+            Energy_Array = np.append(Energy_Array,_State_Energy)
+            State_Array = np.append(State_Array,state.reshape((1,state.size)),axis=0)
+        else:
+            basis.flip(state,index)
+    print("Average Energy: " + str(sum(Energy_Array) / Energy_Array.size))
+    # print(Energy_Array)
+    # print(State_Array[1:])
 
-lattice = tfim.Lattice([15],True)
+lattice = tfim.Lattice([10],True)
 basis = tfim.IsingBasis(lattice)
+
+Monte_Carlo_Sampling(lattice,basis,1,1000000)
 _Energy_Array = Energy_Array(lattice,basis)
 _Probability_Array = Probability_Array(basis,_Energy_Array,1)
-_Magnetization_Sqrd_Array = Magnetization_Sqrd_Array(basis)
-
-
-fileAv = open("ExactAverage.txt","w")
-fileAv.write(str(Average_Energy(basis,_Energy_Array,_Probability_Array)))
-fileAv.close()
-
-fileAv = open("ExactAverageMag.txt","w")
-fileAv.write(str(Average_Magnetization_Sqrd(basis,_Probability_Array,_Magnetization_Sqrd_Array)))
-fileAv.close()
-
-
-
-Tower_Sample_Average_Magnetization_Sqrd(_Probability_Array,_Magnetization_Sqrd_Array,10000)
-Tower_Sample_Average_Energy(_Probability_Array,_Energy_Array,10000)
+print(Average_Energy(basis,_Energy_Array,_Probability_Array))
+# _Magnetization_Sqrd_Array = Magnetization_Sqrd_Array(basis)
+#
+#
+# fileAv = open("ExactAverage.txt","w")
+# fileAv.write(str(Average_Energy(basis,_Energy_Array,_Probability_Array)))
+# fileAv.close()
+#
+# fileAv = open("ExactAverageMag.txt","w")
+# fileAv.write(str(Average_Magnetization_Sqrd(basis,_Probability_Array,_Magnetization_Sqrd_Array)))
+# fileAv.close()
+#
+#
+#
+# Tower_Sample_Average_Magnetization_Sqrd(_Probability_Array,_Magnetization_Sqrd_Array,10000)
+# Tower_Sample_Average_Energy(_Probability_Array,_Energy_Array,10000)
